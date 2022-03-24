cfff250f6af67968711343357dc892bc0650ec4c
AnnaLihanova <anja.lihanova@gmail.com>
2022-03-24 11:07:43 +0300

    old 24.03.2022

---
+++ data/Employee_9.java
@@ -0,0 +1,64 @@
+package pro.sky.coursetwo.data;
+
+import java.util.Objects;
+
+public class Employee_9 {
+//    private final String lastName;
+//    private final String firstName;
+//    private int department;
+//    private int salary;
+//
+//    public Employee_9(String lastName, String firstName, int department, int salary) {
+//        this.lastName = lastName;
+//        this.firstName = firstName;
+//        this.department = department;
+//        this.salary = salary;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public int getDepartment() {
+//        return department;
+//    }
+//
+//    public void setDepartment(int department) {
+//        this.department = department;
+//    }
+//
+//    public int getSalary() {
+//        return salary;
+//    }
+//
+//    public void setSalary(int salary) {
+//        this.salary = salary;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Employee_9{" +
+//                "lastName='" + lastName + '\'' +
+//                ", firstName='" + firstName + '\'' +
+//                ", department=" + department +
+//                ", salary=" + salary +
+//                '}';
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Employee_9 that = (Employee_9) o;
+//        return department == that.department && Float.compare(that.salary, salary) == 0 && lastName.equals(that.lastName) && firstName.equals(that.firstName);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(lastName, firstName, department, salary);
+//    }
+}
